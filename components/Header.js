ee995881878080eeba860214d517d2cfe38b966e
Group B <57354055+GroupB-Project-CaseStudy@users.noreply.github.com>
2021-05-08 16:09:16 +0800

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react' ;
+
+const header = props => {
+    return (
+        <div>
+        <div style={{backgroundColor : 'black' , 
+                    lineHeight : '80px' ,
+                    width : '100%' ,  
+                    color : 'white' ,
+                    fontSize : '30px' ,
+                    textAlign : 'center'}}>
+            Weather App
+        </div>
+        <h1 style={{textAlign : 'center'}}>Ma Li 1727913</h1>
+        </div>
+    );
+}
+
+export default header ;
